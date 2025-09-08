2b262bec9d2f5221ddbd71a228931a19233cf8a4
diixo <anatolii.podoba@gmail.com>
2025-09-08 07:07:59 +0300

    gpt_rpe.py: deleted

--- gpt_rpe.py
+++
@@ -1,7 +0,0 @@
-# https://arxiv.org/abs/2105.08399 (SPE)
-# https://arxiv.org/abs/2205.13401 (URPE)
-
-# SPE: https://github.com/aliutkus/spe
-# URPE: https://github.com/lsj2408/URPE
-
-# TODO: avoid context fading or attention decay 
