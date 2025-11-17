b18998a57720191af522c09e646c2825792b7927
diixo <anatolii.podoba@gmail.com>
2025-11-17 07:27:19 +0200

    fineweb_gpt.py: use gpt-neo-125m

--- fineweb_gpt.py
+++ fineweb_gpt.py
@@ -28,7 +28,8 @@ os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 fw = load_dataset("HuggingFaceFW/fineweb-edu", name="sample-10BT", split="train")
 
 # init the tokenizer
-tokenizer = GPT2TokenizerFast.from_pretrained("data/noomo", local_files_only=True)
+#tokenizer = GPT2TokenizerFast.from_pretrained("data/noomo", local_files_only=True)
+tokenizer = GPT2TokenizerFast.from_pretrained("data/gpt-neo-125m", local_files_only=True)
 
 eot = tokenizer.convert_tokens_to_ids("<|endoftext|>")
 
