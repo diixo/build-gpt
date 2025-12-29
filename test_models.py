269cbebe551ff54086a267362bb2998195da5d45
diixo <anatolii.podoba@gmail.com>
2025-12-29 03:38:05 +0200

    test_models.py

--- test_models.py
+++ test_models.py
@@ -36,11 +36,21 @@ class AutoGPTModel:
         if model_type not in AutoGPTModel.MODEL_MAP:
             raise ValueError(f"Unknown model_type: {model_type}")
 
-        tokenizer = GPT2Tokenizer.from_pretrained("data/gpt2", local_files_only=True)
+        if False:
+            tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-31m")
+            model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-31m")
+            vocab_tok = tokenizer.vocab_size        # pythia-31m=50254
+            vocab_model = model.config.vocab_size   # pythia-31m=50304
+            print(f"Tokenizer vocab size: {vocab_tok}, Model vocab size: {vocab_model}")
+
+            # Find "empty" indices
+            unused_ids = list(range(vocab_tok, vocab_model))
+            print(f"Unallocated tokens (unused ID\'s): {unused_ids[:10]}... all={len(unused_ids)}.")
+
+
         #tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b", local_files_only=True)
         #tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-31m", local_files_only=True)
-        #model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-31m", local_files_only=True)
-        #tokenizer = GPT2TokenizerFast.from_pretrained("data/noomo", local_files_only=True)
+        tokenizer = GPT2Tokenizer.from_pretrained("data/gpt2", local_files_only=True)
 
         # Extract sizes
         vocab_sz = tokenizer.vocab_size # 50257
@@ -48,10 +58,6 @@ class AutoGPTModel:
         print("Vocab size: tokenizer =", vocab_sz)
 
         if True:
-            # Find "empty" indices
-            # unused_ids = list(range(vocab_sz, vocab_model))
-            # print(f"Unallocated tokens (unused ID\'s): {unused_ids[:10]}... all={len(unused_ids)}.")
-
             # Check alls special tokens
             print(f"Special tokens =", tokenizer.special_tokens_map)
 
