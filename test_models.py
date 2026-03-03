eefd989b53900d61a4e3d569a98741b9da2395eb
diixo <anatolii.podoba@gmail.com>
2026-03-03 19:30:38 +0200

    test_models.py + test.txt (updated) to decrease the probability of prompt.

--- test_models.py
+++ test_models.py
@@ -554,7 +554,7 @@ if __name__ == "__main__":
     tokenizer_type = "gpt2"    # "gpt2", "noomo"
 
 
-    train_config = TrainerConfig(epochs=32, batch_size=32, grad_accum_steps=1)
+    train_config = TrainerConfig(epochs=15, batch_size=32, grad_accum_steps=1)
 
     model, tokenizer = load_pretrained_model(model_type, file_path_from_config(model_type, train_config, SAVE_DIRECTORY))
 
