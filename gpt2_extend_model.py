3887c4d9d1f45de642ec5b016d41da7ad18e493a
diixo <anatolii.podoba@gmail.com>
2026-03-10 01:46:03 +0200

    gpt2_extend_model: initial commit + code generation.

---
+++ gpt2_extend_model.py
@@ -0,0 +1,40 @@
+
+from transformers import GPT2TokenizerFast, GPT2LMHeadModel
+
+
+save_dir = "data/gpt2-extended"
+model_name = "gpt2"
+
+
+tokenizer = GPT2TokenizerFast.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+
+
+'''
+USER=QUESTION
+ASSISTANT=ANSWER
+KNOWLEDGE=CONTEXT
+'''
+special_tokens = {
+    "pad_token": "<|pad|>",
+    "additional_special_tokens": [
+        "<|system|>",
+        "<|user|>",
+        "<|assistant|>",
+        "<|knowledge|>",
+        "<|instruction|>",
+        "<|reference|>",
+    ]
+}
+
+#num_added = tokenizer.add_special_tokens(special_tokens)
+#print("added:", num_added)
+print("vocab size:", len(tokenizer))
+
+
+model.resize_token_embeddings(len(tokenizer))
+
+tokenizer.save_pretrained(save_dir)
+
+#model.config.pad_token_id = tokenizer.pad_token_id
+#model.save_pretrained(save_dir)
